3b6896096782ec9d4dfad6fee3a8b92a34c2f319
Dune24 <coolface2009second@gmail.com>
2018-02-28 13:44:45 +0200

    Add functionality

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const container = document.querySelector('#container');
 const createGridBtn = document.querySelector('#create-grid');
-function drawGrid () {
-    for (let i = 1; i <= 16; i++) {
-        for (let j = 1; j <= 16; j++) {
+
+function drawGrid (num, color) {
+    container.innerHTML = "";
+    
+    for (let i = 1; i <= num; i++) {
+        for (let j = 1; j <= num; j++) {
             const squareDiv = document.createElement('div');
+            
             squareDiv.classList.add('square');
             squareDiv.addEventListener('mouseover', () => {
-                squareDiv.style.backgroundColor = 'blue';
+                (color == 'random') ? squareDiv.style.backgroundColor = randomColor() 
+                                    : squareDiv.style.backgroundColor = color;
         });
+            
             container.appendChild(squareDiv);
         }
     }
-  createGridBtn.removeEventListener('click', drawGrid);
+    container.style['grid-template-columns'] = `repeat(${num}, 1fr)`;
 }
 
+function randomColor() {
+    return `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+}
 
-createGridBtn.addEventListener('click', drawGrid);
-
-/*
-const allSquareDivs = document.querySelectorAll('.square');
-allSquareDivs.forEach((div) => {
-    div.addEventListener('mouseover', (e) => {
-       div.style.backgroundColor = 'red';
-  })
-})
-*/
+createGridBtn.addEventListener('click', () => {
+    let squaresPerSide = prompt('Please enter the number of squares per side of the grid:', 16);
+    let squaresColor = prompt('Which color do you want to have? For example: random, green, yellow', 'random');
+    squaresColor.toLowerCase();
+    drawGrid(squaresPerSide, squaresColor);
+});
